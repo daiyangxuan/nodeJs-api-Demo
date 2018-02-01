cea0c3a60933a30317255343620b30f62733dba0
戴熠 <daiyi@zccp.com>
2018-02-01 16:19:16 +0800

    简化params

--- routes/apiDemoRouter.js
+++ routes/apiDemoRouter.js
@@ -6,18 +6,14 @@ const txService = require('./txService');
 router.use(bodyParser.urlencoded({ extended: false }));  
 
 router.post('/createUnit', function(req, res) {
-    // 存证流水号，必填，每次写入都需要更新，存证流水号必须唯一，重复提交会失败
-    let dataUnitId = req.body.dataUnitId;
-    // 前序存证流水号，可为空
-    let preDataUnitId = req.body.preDataUnitId;
-    // 存证hash
-    let existenceHash = req.body.existenceHash;
-    // 自定义字符串
-    let customString = req.body.customString;
-    // 当前操作人身份合约地址
-    let opIdentityAddress = req.body.opIdentityAddress;
     let method = "createUnit";
-    let params = [dataUnitId,preDataUnitId,existenceHash,customString,opIdentityAddress];
+    let params = [
+        req.body.dataUnitId,        // 存证流水号，必填，每次写入都需要更新，存证流水号必须唯一，重复提交会失败
+        req.body.preDataUnitId,     // 前序存证流水号，可为空
+        req.body.existenceHash,     // 存证hash
+        req.body.customString,      // 自定义字符串
+        req.body.opIdentityAddress  // 当前操作人身份合约地址
+    ];
     txService.sendTX(method,params);
 });
 
